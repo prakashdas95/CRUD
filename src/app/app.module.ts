a883a042cb9093711ac9258cf1f69b69e2aef7d9
Prakash <prakashindi01@gmail.com>
2020-01-25 16:49:57 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
-import { DatePipe } from '@angular/common';
+import { DatePipe } from "@angular/common";
 
-
-import { MaterialModule } from './material/material.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from "./material/material.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AngularFireModule } from "angularfire2";
 import { AngularFireDatabaseModule } from "angularfire2/database";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EmployeesComponent } from './employees/employees.component';
-import { EmployeeComponent } from './employees/employee/employee.component';
-import { environment } from './../environments/environment';
-import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
-import { MatConfirmDialogComponent } from './mat-confirm-dialog/mat-confirm-dialog.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { EmployeesComponent } from "./employees/employees.component";
+import { EmployeeComponent } from "./employees/employee/employee.component";
+import { environment } from "./../environments/environment";
+import { EmployeeListComponent } from "./employees/employee-list/employee-list.component";
+import { MatConfirmDialogComponent } from "./mat-confirm-dialog/mat-confirm-dialog.component";
 
 @NgModule({
   declarations: [
@@ -24,8 +23,7 @@ import { MatConfirmDialogComponent } from './mat-confirm-dialog/mat-confirm-dial
     EmployeesComponent,
     EmployeeComponent,
     EmployeeListComponent,
-    MatConfirmDialogComponent,
-
+    MatConfirmDialogComponent
   ],
   imports: [
     BrowserAnimationsModule,
@@ -41,4 +39,4 @@ import { MatConfirmDialogComponent } from './mat-confirm-dialog/mat-confirm-dial
   bootstrap: [AppComponent],
   entryComponents: [EmployeeComponent, MatConfirmDialogComponent]
 })
-export class AppModule { }
+export class AppModule {}
